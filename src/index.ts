5f13832bc0cd1eafa4ffbc0a9fc478f1885c70ee
Sam Sonter <62767142+asapsonter@users.noreply.github.com>
2024-01-04 14:08:48 +0800

    set up params test 2023-1-4

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-console.log(
-  "i dont really like tsc lol "
-);
+import express from 'express';
+
+
+const app = express(); //express var
+
+// get 
+// put 
+// post 
+// del
+app.use(express.json());
+app.delete('/user/:id', (req, res, next) => {
+
+  console.log(req.params.id);
+  return res.send('hello');
+  
+});
+
+app.listen(3000, ()=> console.log("server Open now ")); // server port listening
+
+
+
+
+
