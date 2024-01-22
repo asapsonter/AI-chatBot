e9c245117685b8c5171bf3e65e08551d6ffa9d34
Sam Sonter <62767142+asapsonter@users.noreply.github.com>
2024-01-22 18:39:22 +0800

    del env

--- dist/controllers/user-controllers.js
+++ dist/controllers/user-controllers.js
@@ -11,4 +11,16 @@ export const getAllUsers = async (req, res, next) => {
     }
     ;
 };
+export const userSignup = async (req, res, next) => {
+    // Get all users
+    try {
+        const users = await User.find();
+        return res.status(200).json({ message: "OK", users });
+    }
+    catch (err) {
+        console.error(err);
+        return res.status(200).json({ message: "ERROR", cause: err.message });
+    }
+    ;
+};
 //# sourceMappingURL=user-controllers.js.map
--- src/controllers/user-controllers.ts
+++ src/controllers/user-controllers.ts
@@ -16,3 +16,19 @@ export const getAllUsers = async (
 
     };
 }
+
+export const userSignup = async (
+    req: Request,
+     res: Response, 
+     next: NextFunction) => {
+    // Get all users
+    try {
+        const users = await User.find();
+        return res.status(200).json({message: "OK", users});
+
+    } catch (err) {
+        console.error(err);
+        return res.status(200).json({message: "ERROR", cause: err.message});
+
+    };
+}
